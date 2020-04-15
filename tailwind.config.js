b19bf3797a441b776a4399a9cd9f47f7a744b91a
Duane <duane@codewheel.co.uk>
2020-04-15 18:45:40 +0100

    Part 1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "cw-grey-050" : "F0F4FB",
+        "cw-grey-100": "#D9E2EC",
+        "cw-grey-700": "#334E68",
+        "cw-grey-800": "#243B53",
+        "cw-grey-900": "#102A43",
+        "cw-blue-300": "#62B0E8",
+        "cw-blue-400": "#4098D7",
+        "cw-blue-600": "#186FAF",
+      },
+      fontFamily: {
+      'roboto' :['Roboto', 'sans-serif'],
+      'work-sans' :['Work Sans', 'sans-serif'],
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
